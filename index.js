2c6ebc5653fcdcee6d1e19ac576de6d32cc4fbaf
B33 <geoffrey.bii@student.moringaschool.com>
2023-07-07 19:27:16 +0300

    assignemet submission

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 /* Your Code Here */
 
 /*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
+//  We're giving you this function. Take a look at it, you might see some usage
+//  that's new and different. That's because we're avoiding a well-known, but
+//  sneaky bug that we'll cover in the next few lessons!
 
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
+//  As a result, the lessons for this function will pass *and* it will be available
+//  for you to use if you need it!
+//  */
 
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
@@ -20,4 +20,16 @@ const allWagesFor = function () {
 
     return payable
 }
+let testEmployee = createEmployeeRecord(["Gray", "Worm", "Security", 1])
 
+function createEmployeeRecord(array) {
+    // testEmployee.firstName= testEmployee[0]
+    array.firstName = array[0];
+    array.familyName = array[1];
+    array.title = array[2];
+    array.payPerHour = array[3];
+   
+   
+    
+}
+console.log(createEmployeeRecord(testEmployee));
